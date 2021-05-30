90f3a2df16efd217bc1d15ef7eda547deafd3cf6
Ignacio Rodrigo Galieri <ignacio.galieri@nubicall.com>
2021-05-30 02:06:45 -0500

    nacho_nerd - enabled rgb animation - define caplock layer rgb

--- keyboards/planck/keymaps/nacho_nerd/config.h
+++ keyboards/planck/keymaps/nacho_nerd/config.h
@@ -32,4 +32,6 @@
 
 // Most tactile encoders have detents every 4 stages
 #define ENCODER_RESOLUTION 4
+#define RGBLIGHT_LAYERS
+#define RGBLIGHT_ANIMATIONS
 
--- keyboards/planck/keymaps/nacho_nerd/keymap.c
+++ keyboards/planck/keymaps/nacho_nerd/keymap.c
@@ -286,3 +286,21 @@ bool music_mask_user(uint16_t keycode) {
             return true;
     }
 }
+
+// RGB LAYER
+const rgblight_segment_t PROGMEM capslock_layer[] = RGBLIGHT_LAYER_SEGMENTS({0, 9, HSV_RED});
+
+const rgblight_segment_t* const PROGMEM my_rgb_layers[] = RGBLIGHT_LAYERS_LIST(
+    capslock_layer
+);
+
+void keyboard_post_init_user(void) {
+    // Enable the LED layers
+    rgblight_layers = my_rgb_layers;
+}
+
+bool led_update_user(led_t led_state) {
+    // Turn on RBG for capslock.
+    rgblight_set_layer_state(0, led_state.caps_lock);
+    return true;
+}
